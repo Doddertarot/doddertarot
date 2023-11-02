90bc6d4f3428da9b171ec3ea6aa4b96cc310c4df
yaj55billy <yaj55ushydna@gmail.com>
2023-11-02 20:34:08 +0800

    index bottom bg

--- pages/index.vue
+++ pages/index.vue
@@ -29,18 +29,23 @@ const testfunc = () => {
 					抽牌
 				</button>
 			</div>
-			<section class="section">
-				<p>抽牌前：</p>
-				<p>
-					找一個安靜訊號好的環境，開始抽牌前，放鬆心情深呼吸，集中精神，冥想您所要問的問題，問題越清晰，塔羅牌更能清楚的指引出答案。
-				</p>
-			</section>
-			<section class="section">
-				<p>抽牌後：</p>
-				<p>
-					接受塔羅牌給你的指引，結果不是絕對，也許無法給出你心中想要的答案，請誠實的面對塔羅牌給的建議，塔羅牌會帶你看出問題的癥結點，決定權在你手上，做出對自己最好的安排。
-				</p>
-			</section>
+			<div class="index__bottom">
+				<section class="section">
+					<p>抽牌前：</p>
+					<p>
+						找一個安靜訊號好的環境，開始抽牌前，放鬆心情深呼吸，集中精神，冥想您所要問的問題，問題越清晰，塔羅牌更能清楚的指引出答案。
+					</p>
+				</section>
+				<section class="section">
+					<p>抽牌後：</p>
+					<p>
+						接受塔羅牌給你的指引，結果不是絕對，也許無法給出你心中想要的答案，請誠實的面對塔羅牌給的建議，塔羅牌會帶你看出問題的癥結點，決定權在你手上，做出對自己最好的安排。
+					</p>
+				</section>
+				<div class="index__bottom__logo">
+					<img src="/img/index_bg.jpg" alt="" />
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
